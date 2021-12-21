3ca657cbee3feb1e98d9a22b0e490df19225fc25
shivaraj shankar <51598598+shivaraj65@users.noreply.github.com>
2021-12-21 14:18:39 +0530

    initial layouts added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import './App.css'
+
+// layouts imports
+import Landing from './layouts/landing/landing'
+import Background from './layouts/background/background'
+
 
 function App() {
+const [page, setpage] = useState(0)
+
   return (
     <div className="App">
+        { page===0? <Landing/>:null }
+        { page===1? <Background/>:null}
+        { page===2? null:null }
+        { page===3? null:null }
+        { page===4? null:null }
+        { page===5? null:null }
       
     </div>
   );
