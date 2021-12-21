84d0f0205a53ce13fee7f49874f034b2ce990b4c
shivaraj shankar <51598598+shivaraj65@users.noreply.github.com>
2021-12-21 14:11:36 +0530

    boilerplate

---
+++ src/layouts/background/background.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Background=()=>{
+    return(
+        <>
+
+        </>
+    
+    )
+}
+
+export default React.memo(Backgrounda)
---
+++ src/layouts/landing/landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Landing =()=>{
+    return(
+        <>
+
+
+        </>
+    )
+}
+
+export default React.memo(Landing)
